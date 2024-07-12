40187b7ca40b5ffb3c95a6b98ca85a9c08e44fd1
Surendran E <surenethiraj@gmail.com>
2024-07-12 15:41:08 +0530

    Update Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     localStorage.setItem("TODO", JSON.stringify(tasks));
   }, [tasks]);
 
-  ## Added Comment
+  ## Added Comment - Modified
   function updateTasks(updatedTasks) {
     setTasks(updatedTasks);
   }
